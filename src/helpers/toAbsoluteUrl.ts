d7844701073974cc09289322722dc4898cc21d00
Julian Popescu <jpopesculian@gmail.com>
2018-10-15 21:29:33 +0200

    fix bug with mixed content

--- src/helpers/toAbsoluteUrl.ts
+++ src/helpers/toAbsoluteUrl.ts
@@ -1,4 +1,10 @@
+import { last, split, includes } from "lodash/fp";
+
 export const toAbsoluteUrl = (url: string): string => {
+  const file = last(split("/", url));
+  if (!includes(".", file) && last(url) !== "/") {
+    url += "/"; // append trailing slash otherwise github.io pages redirect to insecure content
+  }
   if (window.URL) {
     return new URL(url, window.location.href).href;
   }
